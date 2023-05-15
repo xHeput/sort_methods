527dbe30a7afbd1b56f24ae566bb9ac47389a3d7
xHeput <133649025+xHeput@users.noreply.github.com>
2023-05-15 12:29:54 +0200

    Add files via upload

---
+++ BubbleSort_losoweliczbyczas.py
@@ -0,0 +1,84 @@
+from numpy import random
+from time import perf_counter_ns
+import matplotlib.pyplot as plt
+
+
+# BubbleSort
+def BubbleSort(macierz):
+    for i in range(len(macierz)):
+
+        for j in range(0, len(macierz) - i - 1):
+
+            if macierz[j] > macierz[j + 1]:
+                fagata = macierz[j]
+                macierz[j] = macierz[j + 1]
+                macierz[j + 1] = fagata
+
+
+x = 100
+sumar = 0
+for lp in range(100):
+    liczby = random.randint(-5000, 5000, size=x)
+    start = perf_counter_ns()
+    BubbleSort(liczby)
+    stop = perf_counter_ns()
+    # print("Wygenerowanie liczb zaczeło się: ", start, "Skończyło się: ", stop,)
+    # print("Całosc trwała: ", stop-start, "ns")
+    roznica = stop - start
+    sumar += roznica
+
+srednia_100 = sumar / 100
+print("sredni czas itereacji", x, "elementów to: ", srednia_100)
+
+x = 500
+sumar = 0
+for lp in range(100):
+    liczby = random.randint(-5000, 5000, size=x)
+    start = perf_counter_ns()
+    BubbleSort(liczby)
+    stop = perf_counter_ns()
+    # print("Wygenerowanie liczb zaczeło się: ", start, "Skończyło się: ", stop,)
+    # print("Całosc trwała: ", stop-start, "ns")
+    roznica = stop - start
+    sumar += roznica
+
+srednia_500 = sumar / 100
+print("sredni czas itereacji", x, "elementów to: ", srednia_500)
+
+x = 1000
+sumar = 0
+for lp in range(100):
+    liczby = random.randint(-5000, 5000, size=x)
+    start = perf_counter_ns()
+    BubbleSort(liczby)
+    stop = perf_counter_ns()
+    # print("Wygenerowanie liczb zaczeło się: ", start, "Skończyło się: ", stop,)
+    # print("Całosc trwała: ", stop-start, "ns")
+    roznica = stop - start
+    sumar += roznica
+
+srednia_1000 = sumar / 100
+print("sredni czas itereacji", x, "elementów to: ", srednia_1000)
+# Use only when you have lots of time
+# x = 5000
+# sumar = 0
+# for lp in range(100):
+    # liczby = random.randint(-5000, 5000, size=x)
+    # start = perf_counter_ns()
+    # BubbleSort(liczby)
+    # stop = perf_counter_ns()
+    # print("Wygenerowanie liczb zaczeło się: ", start, "Skończyło się: ", stop,)
+    # print("Całosc trwała: ", stop-start, "ns")
+    # roznica = stop - start
+    # sumar += roznica
+
+# srednia_5000 = sumar / 100
+# print("sredni czas itereacji", x, "elementów to: ", srednia_5000)
+
+srednia = [srednia_100, srednia_500, srednia_1000]#, srednia_5000]
+XD = [100, 500, 1000]#, 5000]
+plt.plot(srednia, XD)
+plt.xlabel("Czas w nanosekundach")
+plt.ylabel("ilość losowań")
+plt.title("Czas obliczeń poszczególnych sortowań z wykorzystaniem algorytmu Bubble Sort")
+plt.show()
